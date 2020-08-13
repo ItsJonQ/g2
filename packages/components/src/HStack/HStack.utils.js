5ca86fa260569d992b32527a444ec5f08cb730d6
Jon Q <hello@jonquach.com>
2020-08-12 20:23:12 -0400

    Update HStack align

--- packages/components/src/HStack/HStack.utils.js
+++ packages/components/src/HStack/HStack.utils.js
@@ -1,9 +1,13 @@
 const ALIGNMENTS = {
-	bottom: { align: 'flex-end' },
-	center: { align: 'center' },
-	left: { justify: 'flex-start' },
-	right: { justify: 'flex-start' },
-	top: { align: 'flex-start' },
+	bottom: { align: 'flex-end', justify: 'center' },
+	bottomLeft: { align: 'flex-start', justify: 'flex-end' },
+	bottomRight: { align: 'flex-end', justify: 'flex-end' },
+	center: { align: 'center', justify: 'center' },
+	left: { align: 'center', justify: 'flex-start' },
+	right: { align: 'center', justify: 'flex-start' },
+	top: { align: 'flex-start', justify: 'center' },
+	topLeft: { align: 'flex-start', justify: 'flex-start' },
+	topRight: { align: 'flex-start', justify: 'flex-end' },
 };
 
 export function getAlignmentProps(alignment) {
