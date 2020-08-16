a1d050656108e547e325090bf945185329ced497
Jon Q <hello@jonquach.com>
2020-08-16 12:07:05 -0400

    Try using SortableJS

---
+++ packages/components/src/Draggable/__stories__/Sortable.stories.js
@@ -0,0 +1,82 @@
+import { useListState } from '@wp-g2/utils';
+import faker from 'faker';
+import { Schema } from 'faker-schema';
+import React, { createRef } from 'react';
+import SortableCore from 'sortablejs';
+
+import {
+	Card,
+	CardBody,
+	Heading,
+	Subheading,
+	Text,
+	View,
+	VStack,
+} from '../../index';
+
+export default {
+	title: 'Components/Draggable',
+};
+
+class Sortable extends React.Component {
+	nodeRef = createRef();
+	sortable = null;
+
+	componentDidMount() {
+		this.sortable = SortableCore.create(this.nodeRef.current, {
+			animation: 150,
+			forceFallback: true,
+			ghostClass: 'ghost',
+		});
+	}
+
+	componentWillUnmount() {
+		this.sortable.destroy();
+	}
+
+	render() {
+		return <View ref={this.nodeRef}>{this.props.children}</View>;
+	}
+}
+
+const Draggable = (props) => {
+	return (
+		<View
+			data-sortable-item="true"
+			{...props}
+			css={[{ marginBottom: 8 }, `&.ghost {opacity: 0}`]}
+		/>
+	);
+};
+
+const itemSchema = new Schema(() => ({
+	id: faker.random.uuid(),
+	name: faker.name.firstName(),
+}));
+
+const Example = () => {
+	const [items] = useListState(itemSchema.make(10));
+	return (
+		<VStack spacing={5}>
+			<VStack spacing={0}>
+				<Heading>SortableJS</Heading>
+				<Subheading>Probaby a no go.</Subheading>
+			</VStack>
+			<Sortable>
+				{items.map((item) => (
+					<Draggable key={item.id}>
+						<Card>
+							<CardBody>
+								<Text>{item.name}</Text>
+							</CardBody>
+						</Card>
+					</Draggable>
+				))}
+			</Sortable>
+		</VStack>
+	);
+};
+
+export const SortableExample = () => {
+	return <Example />;
+};
--- packages/components/src/View/View.js
+++ packages/components/src/View/View.js
@@ -1,3 +1,32 @@
+import { connect } from '@wp-g2/context';
 import { BaseView } from '@wp-g2/styles';
+import { is, warning } from '@wp-g2/utils';
+import React from 'react';
 
-export default BaseView;
+import { Text } from '../Text';
+
+function View({ children, forwardedRef, ...props }) {
+	warning(
+		is.string(children),
+		'@wp-g2/components',
+		'View',
+		`children prop should not be a string.`,
+		`To render text, use the <Text /> component.`,
+		`This instance of <View /> will be using <Text /> instead.`,
+	);
+
+	if (is.string(children)) {
+		return (
+			<Text {...props} ref={forwardedRef}>
+				{children}
+			</Text>
+		);
+	}
+
+	return (
+		<BaseView {...props} ref={forwardedRef}>
+			{children}
+		</BaseView>
+	);
+}
+export default connect(View);
