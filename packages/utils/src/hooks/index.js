de40534091940a5ec85e1df1d8422b2c3d6a6d18
Jon Q <hello@jonquach.com>
2020-08-17 01:25:01 -0400

    Update deps

--- packages/utils/src/hooks/index.js
+++ packages/utils/src/hooks/index.js
@@ -1,3 +1,4 @@
+export * from './useBoolState';
 export * from './useControlledState';
 export * from './useListState';
 export * from './useLocalState';
---
+++ packages/utils/src/hooks/useBoolState.js
@@ -0,0 +1,1 @@
+export { useBoolState } from 'use-enhanced-state';
