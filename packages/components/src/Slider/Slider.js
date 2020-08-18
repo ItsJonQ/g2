5cbbe25651c23a9054188b4d560d21c01564ac8f
Jon Q <hello@jonquach.com>
2020-08-18 16:36:05 -0400

    Fix slider + form group

--- packages/components/src/Slider/Slider.js
+++ packages/components/src/Slider/Slider.js
@@ -2,12 +2,14 @@ import { connect } from '@wp-g2/context';
 import { clamp, noop, useControlledState } from '@wp-g2/utils';
 import React from 'react';
 
+import { useFormGroupContext } from '../FormGroup';
 import * as styles from './Slider.styles';
 
 const { SliderView } = styles;
 
 function Slider({
 	onChange = noop,
+	id: idProp,
 	max = 100,
 	min = 0,
 	size = 'medium',
@@ -16,6 +18,8 @@ function Slider({
 	...props
 }) {
 	const [value, setValue] = useControlledState(valueProp, { initial: 50 });
+	const { id: contextId } = useFormGroupContext();
+	const id = idProp || contextId;
 
 	const handleOnChange = (event) => {
 		const next = parseFloat(event.target.value);
@@ -35,6 +39,7 @@ function Slider({
 		<SliderView
 			{...props}
 			cx={cx}
+			id={id}
 			max={max}
 			min={min}
 			onChange={handleOnChange}
