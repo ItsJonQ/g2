03fc30889925190d3920f87ed55b481054a60b2d
Jon Q <hello@jonquach.com>
2020-08-25 09:31:56 -0400

    ComponentInspector: Adjust border for namespaced components

--- packages/components/src/ComponentInspector/ComponentInspector.styles.js
+++ packages/components/src/ComponentInspector/ComponentInspector.styles.js
@@ -3,15 +3,22 @@ import { cns, ns, styled } from '@wp-g2/styles';
 const [CONNECTED_NAMESPACE] = Object.keys(cns());
 const [NAMESPACE] = Object.keys(ns());
 
-const componentSelector = `[${CONNECTED_NAMESPACE}]:not([${NAMESPACE}='Debugger']):not([${NAMESPACE}='ComponentInspector'])`;
+const connectedComponentSelector = `[${CONNECTED_NAMESPACE}]:not([${NAMESPACE}='Debugger']):not([${NAMESPACE}='ComponentInspector'])`;
+const namespacedComponentSelector = `[${NAMESPACE}]:not([${NAMESPACE}='Debugger']):not([${NAMESPACE}='ComponentInspector'])`;
 
 export const ComponentInspectorView = styled.div`
 	&:not([disabled]) {
-		${componentSelector} {
+		&:hover {
+			${namespacedComponentSelector} {
+				outline: 1px solid rgba(255, 0, 0, 0.12);
+			}
+		}
+
+		${connectedComponentSelector} {
 			&:hover {
 				outline: 1px solid rgba(0, 180, 255, 0.2) !important;
 
-				${componentSelector} {
+				${connectedComponentSelector} {
 					outline: 1px solid rgba(0, 180, 255, 0.2) !important;
 
 					&:hover {
