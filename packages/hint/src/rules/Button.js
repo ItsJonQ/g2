6e62d15d3172e226e4fdf4446d2b0847b895aa37
Jon Q <hello@jonquach.com>
2020-08-26 09:52:55 -0400

    Enable Buttons to account for Grid

--- packages/hint/src/rules/Button.js
+++ packages/hint/src/rules/Button.js
@@ -2,17 +2,19 @@ export default {
 	rules: {
 		'button-siblings-layout-wrap': {
 			create: (context) => {
-				return (node, { getComponentName }) => {
+				return (node, { getClosestComponent, getComponentName }) => {
 					const isButton = getComponentName(node) === 'Button';
 					if (!isButton) return;
 
 					const sibling = node.nextElementSibling;
 					const isNextButton = getComponentName(sibling) === 'Button';
-					if (isNextButton) {
+					const gridWrapper = getClosestComponent(node, 'Grid');
+
+					if (isNextButton && !gridWrapper) {
 						context.report({
 							title: 'Button / Siblings: Layout wrap',
 							message:
-								'Buttons should not be next to each other. Wrap them with a Layout container, such as Flex, HStack, or VStack.',
+								'Buttons should not be next to each other. Wrap them with a Layout container, such as Grid, Flex, HStack, or VStack.',
 						});
 					}
 				};
