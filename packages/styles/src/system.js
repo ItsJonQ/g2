7d6e2753b0cb3511e59fcb9369697d7c01557611
Jon Q <hello@jonquach.com>
2020-09-04 00:02:00 -0400

    Fix base fontFamily and fontSize to use variables. Update snapshots

--- packages/styles/src/system.js
+++ packages/styles/src/system.js
@@ -1,4 +1,4 @@
-import { createStyleSystem } from '@wp-g2/create-styles';
+import { createStyleSystem, get as getConfig } from '@wp-g2/create-styles';
 
 import {
 	config,
@@ -11,8 +11,8 @@ const systemConfig = {
 	baseStyles: {
 		MozOsxFontSmoothing: 'grayscale',
 		WebkitFontSmoothing: 'antialiased',
-		fontFamily: config.fontFamily,
-		fontSize: config.fontSize,
+		fontFamily: getConfig('fontFamily'),
+		fontSize: getConfig('fontSize'),
 		margin: 0,
 	},
 	config,
