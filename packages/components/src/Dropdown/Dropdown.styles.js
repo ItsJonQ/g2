edd0f7dea9c25dd71841bb9940735cd3a0715114
Jon Q <hello@jonquach.com>
2020-09-12 12:42:30 -0400

    Dropdown: Remove margin style, which was causing issues with Reakit/Menu (Popper.js)

--- packages/components/src/Dropdown/Dropdown.styles.js
+++ packages/components/src/Dropdown/Dropdown.styles.js
@@ -2,15 +2,12 @@ import { css, get, ui } from '@wp-g2/styles';
 
 export const DropdownMenu = css`
 	${ui.zIndex('Dropdown', 9998)};
-	margin-top: -10px;
 	min-width: 200px;
 	opacity: 0;
 	transform-origin: top center;
-	transition: opacity ${get('transitionDurationFastest')} ease,
-		margin-top ${get('transitionDurationFastest')} ease;
+	transition: opacity ${get('transitionDurationFastest')} ease;
 
 	&[data-enter] {
-		margin-top: 0px;
 		opacity: 1;
 	}
 `;
