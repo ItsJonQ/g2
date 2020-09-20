ad9f0a06af0bc68e827bbd5ef1132c3c259cf536
Jon Q <hello@jonquach.com>
2020-09-20 00:11:12 -0400

    Add Popover types

---
+++ packages/components/types/components/Popover.d.ts
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import { CSS, PolymorphicComponent, PopperProps } from './_shared';
+
+export declare type PopoverProps = PopperProps & {
+	/**
+	 * Determines if `Popover` has animations.
+	 */
+	animated?: boolean;
+	/**
+	 * The duration of `Popover` animations.
+	 *
+	 * @default 160
+	 */
+	animationDuration?: boolean;
+	/**
+	 * ID that will serve as a base for all the items IDs.
+	 *
+	 * @see https://reakit.io/docs/Popover/#usePopoverstate
+	 */
+	baseId?: string;
+	/**
+	 * Content to render within the `Popover` floating label.
+	 */
+	content?: React.Component | string;
+	/**
+	 * Renders `Elevation` styles for the `Popover`.
+	 *
+	 * @default 5
+	 */
+	elevation?: number;
+	/**
+	 * Whether or not the dialog should be rendered within `Portal`. It's true by default if modal is true.
+	 *
+	 * @default true
+	 *
+	 * @see https://reakit.io/docs/Popover/#Popover
+	 */
+	modal?: boolean;
+	/**
+	 * Max-width for the `Popover` element.
+	 */
+	maxWidth?: CSS['maxWidth'];
+	/**
+	 * Callback for when the `visible` state changes.
+	 */
+	onVisibleChange?: (...args: any) => void;
+	/**
+	 * Element that triggers the `visible` state of `Popover` when clicked.
+	 *
+	 * @example
+	 * ```jsx
+	 * <Popover trigger={<Button>Greet</Button>}>
+	 *  <Text>Hi! I'm Olaf!</Text>
+	 * </Popover>
+	 * ```
+	 */
+	trigger?: React.Component;
+	/**
+	 * Whether `Popover` is visible.
+	 *
+	 * @default false
+	 *
+	 * @see https://reakit.io/docs/Popover/#usePopoverstate
+	 */
+	visible?: boolean;
+};
+
+/**
+ * `Popover` is a component that provides context and controls in an
+ *
+ * @example
+ * ```jsx
+ * <Popover trigger={<Button>Greet</Button>}>
+ *  <Text>Hi! I'm Olaf!</Text>
+ * </Popover>
+ * ```
+ */
+export declare const Popover: PolymorphicComponent<PopoverProps>;
--- packages/components/types/components/index.d.ts
+++ packages/components/types/components/index.d.ts
@@ -28,6 +28,7 @@ export * from './Image';
 export * from './Initials';
 export * from './Lozenge';
 export * from './Placeholder';
+export * from './Popover';
 export * from './Portal';
 export * from './Radio';
 export * from './RadioGroup';
