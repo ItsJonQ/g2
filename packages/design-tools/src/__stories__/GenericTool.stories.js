1d4b5834590e77b8ab40e74dbb967efa7789feb8
Jon Q <hello@jonquach.com>
2020-09-26 23:43:19 -0400

    add GenericTool perf example (using Jotai)

---
+++ packages/design-tools/src/__stories__/GenericTool.stories.js
@@ -0,0 +1,167 @@
+import {
+	Button,
+	Card,
+	CardBody,
+	Container,
+	FormGroup,
+	Grid,
+	HStack,
+	ListGroup,
+	ListGroupHeader,
+	Slider,
+	Surface,
+	TextInput,
+	VStack,
+} from '@wp-g2/components';
+import { Schema } from '@wp-g2/protokit';
+import { css, styled, ui } from '@wp-g2/styles';
+import { atom, Provider, useAtom } from 'jotai';
+import React, { useState } from 'react';
+
+export default {
+	title: 'DesignTools/GenericTool/PerformanceTest',
+};
+
+const Frame = styled(Surface)`
+	align-items: center;
+	bottom: 0;
+	display: flex;
+	justify-content: center;
+	left: 0;
+	position: fixed;
+	right: 0;
+	top: 0;
+`;
+
+const dimensionSchema = new Schema(() => ({
+	title: '',
+	x: 0,
+	y: 1,
+	z: 0,
+}));
+
+const SliderTextInput = React.memo(({ id, prop, value }) => {
+	const { updateDimension: atom } = useDimensionsContext();
+	const [, updateDimension] = useAtom(atom);
+
+	const handleOnChange = React.useCallback(
+		(next) => {
+			updateDimension({ id, prop, value: next });
+		},
+		[updateDimension, prop, id],
+	);
+
+	return (
+		<Grid>
+			<Slider onChange={handleOnChange} value={value} />
+			<TextInput onChange={handleOnChange} type="number" value={value} />
+		</Grid>
+	);
+});
+
+const DimensionCard = React.memo(({ id, x, y, z }) => {
+	return (
+		<Card>
+			<CardBody>
+				<ListGroup>
+					<ListGroupHeader>Dimensions</ListGroupHeader>
+					<FormGroup label="x">
+						<SliderTextInput id={id} prop="x" value={x} />
+					</FormGroup>
+					<FormGroup label="y">
+						<SliderTextInput id={id} prop="y" value={y} />
+					</FormGroup>
+					<FormGroup label="z">
+						<SliderTextInput id={id} prop="z" value={z} />
+					</FormGroup>
+				</ListGroup>
+			</CardBody>
+		</Card>
+	);
+});
+
+const DimensonsContext = React.createContext({ dimensions: [] });
+const useDimensionsContext = () => React.useContext(DimensonsContext);
+
+const DimensionsList = () => {
+	const { dimensions: atom } = useDimensionsContext();
+	const [dimensions] = useAtom(atom);
+
+	return (
+		<ListGroup>
+			{dimensions.map((item) => {
+				return <DimensionCard {...item} />;
+			})}
+		</ListGroup>
+	);
+};
+
+const AddDimensionsButton = () => {
+	const { addDimension: atom } = useDimensionsContext();
+	const [, addDimension] = useAtom(atom);
+
+	return (
+		<Button onClick={addDimension} variant="primary">
+			Add New
+		</Button>
+	);
+};
+
+const Example = () => {
+	const dimensions = atom([...dimensionSchema.make(2)]);
+
+	const updateDimension = atom(
+		(get) => get(dimensions),
+		(get, set, _arg) => {
+			const { id, prop, value } = _arg;
+			set(
+				dimensions,
+				get(dimensions).map((item) => {
+					if (item.id !== id) return item;
+					return { ...item, [prop]: value };
+				}),
+			);
+		},
+	);
+
+	const addDimension = atom(
+		(get) => get(dimensions),
+		(get, set) => {
+			set(dimensions, [...get(dimensions), dimensionSchema.makeOne()]);
+		},
+	);
+
+	const contextValue = React.useMemo(
+		() => ({
+			addDimension,
+			updateDimension,
+			dimensions,
+		}),
+		[addDimension, dimensions, updateDimension],
+	);
+
+	return (
+		<>
+			<Container
+				css={css([ui.position.relative, ui.zIndex(2)])}
+				width={600}
+			>
+				<DimensonsContext.Provider value={contextValue}>
+					<Provider>
+						<VStack>
+							<HStack>
+								<AddDimensionsButton />
+							</HStack>
+							<DimensionsList />
+						</VStack>
+					</Provider>
+				</DimensonsContext.Provider>
+			</Container>
+			<Frame variant="dotted" />
+		</>
+	);
+};
+
+export const _default = () => {
+	return <Example />;
+};
