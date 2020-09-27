837500ee4be75cdfc19298df0e2baf5f33f779e2
Jon Q <hello@jonquach.com>
2020-09-27 11:59:53 -0400

    Add createBaseStore export from zustand

--- packages/utils/types/index.d.ts
+++ packages/utils/types/index.d.ts
@@ -1,6 +1,6 @@
 export { default as hoistNonReactStatics } from 'hoist-non-react-statics';
 export { default as deepEqual } from 'fast-deep-equal';
 export { default as useResizeAware } from 'react-resize-aware';
-export { default as createStore } from 'zustand';
-export { default as shallowCompare } from 'zustand/shallow';
 export * from 'use-enhanced-state';
+
+export * from './createStore';
