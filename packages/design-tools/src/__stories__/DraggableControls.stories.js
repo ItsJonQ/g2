389b646465ad433c5ff151c1a59e83dd87c2aeb7
Jon Q <hello@jonquach.com>
2020-09-29 23:56:26 -0400

    Add DraggableControl examples

---
+++ packages/design-tools/src/__stories__/DraggableControls.stories.js
@@ -0,0 +1,253 @@
+import { useDragControls, useMotionValue } from '@wp-g2/animations';
+import {
+	Animated,
+	Badge,
+	Button,
+	Card,
+	CardBody,
+	ColorCircle,
+	ColorPicker,
+	Container,
+	Elevation,
+	FormGroup,
+	Grid,
+	HStack,
+	ListGroup,
+	ListGroupHeader,
+	ListGroups,
+	MenuItem,
+	Popover,
+	Slider,
+	Spacer,
+	Subheading,
+	Surface,
+	Text,
+	TextInput,
+	View,
+	VStack,
+} from '@wp-g2/components';
+import { FiMinus, FiPlus } from '@wp-g2/icons';
+import { Schema } from '@wp-g2/protokit';
+import { css, styled, ui } from '@wp-g2/styles';
+import isEqual from 'lodash/isEqual';
+import React from 'react';
+import * as yup from 'yup';
+
+export default {
+	title: 'DesignTools/DraggableControls',
+};
+
+const TouchPadControl = ({ circlePositionX, circlePositionY }) => {
+	const dots = Array.from({ length: 250 });
+
+	return (
+		<VStack
+			alignment="center"
+			css={`
+				align-items: center;
+			`}
+			expanded={false}
+		>
+			<Subheading>TouchPad Control</Subheading>
+			<Animated
+				drag
+				dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+				dragElastic={0}
+				dragMomentum={false}
+				onDrag={(event, info) => {
+					circlePositionX.set(info.offset.x);
+					circlePositionY.set(info.offset.y);
+				}}
+				onDragEnd={() => {
+					circlePositionX.set(0);
+					circlePositionY.set(0);
+				}}
+			>
+				<Card
+					backgroundSize={12}
+					css={`
+						cursor: pointer;
+						width: 240px;
+						height: 100px;
+						user-select: none;
+
+						&:active {
+							cursor: move;
+						}
+					`}
+					elevation={2}
+					variant="dotted"
+				>
+					<Grid
+						columns={25}
+						css={`
+							width: 240px;
+							height: 100px;
+							overflow: hidden;
+							border-radius: 8px;
+							user-select: none;
+						`}
+						gap={'0px'}
+					>
+						{dots.map((i, index) => {
+							return (
+								<View
+									css={[
+										ui.frame.width(8),
+										ui.frame.height(8),
+										ui.hover([
+											ui.background.admin,
+											`transition: all 0.1s ease`,
+										]),
+										ui.opacity(0.3),
+										`transition: all 2s ease`,
+									]}
+									key={index}
+								/>
+							);
+						})}
+					</Grid>
+				</Card>
+			</Animated>
+		</VStack>
+	);
+};
+
+const JoystickControl = ({ circlePositionX, circlePositionY }) => {
+	const constraintsRef = React.useRef(null);
+	const py = useMotionValue(0);
+	const px = useMotionValue(0);
+
+	return (
+		<VStack
+			css={`
+				align-items: center;
+			`}
+			expanded={false}
+		>
+			<Subheading>Joystick Control</Subheading>
+			<Surface
+				border
+				css={[
+					`
+						cursor: pointer;
+						width: 60px;
+						height: 60px;
+          `,
+					ui.borderRadius.circle,
+				]}
+				ref={constraintsRef}
+			>
+				<View
+					css={css([
+						ui.alignment.content.center,
+						ui.frame.height('100%'),
+						ui.position.relative,
+						ui.zIndex(2),
+					])}
+				>
+					<Animated
+						drag
+						dragConstraints={{
+							top: 0,
+							bottom: 0,
+							left: 0,
+							right: 0,
+						}}
+						dragElastic={0.05}
+						dragMomentum={false}
+						onDrag={(event, info) => {
+							circlePositionX.set(info.offset.x);
+							circlePositionY.set(info.offset.y);
+						}}
+						onDragEnd={() => {
+							circlePositionX.set(0);
+							circlePositionY.set(0);
+						}}
+						style={{
+							x: px,
+							y: py,
+						}}
+					>
+						<Surface
+							border
+							css={[
+								`
+								cursor: pointer;
+								width: 48px;
+                height: 48px;
+                position: relative;
+                padding: 6px;
+
+                &:active {
+                  cursor: move;
+                }
+              `,
+								ui.borderRadius.circle,
+								ui.zIndex(2),
+							]}
+						>
+							<View
+								css={[
+									ui.borderRadius.circle,
+									{
+										boxShadow:
+											'0 10px 10px rgba(0, 0, 0, 0.1) inset',
+										height: '100%',
+									},
+								]}
+							/>
+							<Elevation borderRadius={99999} value={10} />
+						</Surface>
+					</Animated>
+				</View>
+			</Surface>
+		</VStack>
+	);
+};
+
+const Example = () => {
+	const circlePositionX = useMotionValue(0);
+	const circlePositionY = useMotionValue(0);
+
+	return (
+		<Grid
+			css={`
+				position: fixed;
+				top: 0;
+				bottom: 0;
+				left: 0;
+				right: 0;
+			`}
+			gap={0}
+		>
+			<Surface borderRight css={ui.padding(5)}>
+				<VStack expanded={false} spacing={8}>
+					<TouchPadControl
+						circlePositionX={circlePositionX}
+						circlePositionY={circlePositionY}
+					/>
+					<JoystickControl
+						circlePositionX={circlePositionX}
+						circlePositionY={circlePositionY}
+					/>
+				</VStack>
+			</Surface>
+			<Surface css={[ui.padding(5), ui.alignment.center]}>
+				<Animated
+					css={[
+						ui.background.admin,
+						ui.frame.width(100),
+						ui.frame.height(100),
+						ui.borderRadius.circle,
+					]}
+					style={{ x: circlePositionX, y: circlePositionY }}
+				/>
+			</Surface>
+		</Grid>
+	);
+};
+
+export const _default = () => {
+	return <Example />;
+};
