f8edde7bafc4c84db3a1942dd1aa355b5c35c294
Jon Surrell <jon.surrell@gmail.com>
2020-10-13 16:05:02 +0200

    Fix based on zustand type

--- packages/substate/src/useSubState.js
+++ packages/substate/src/useSubState.js
@@ -1,6 +1,23 @@
 import { useRef } from 'react';
 import createStore from 'zustand';
 
-export function useSubState(...args) {
-	return useRef(createStore(...args)).current;
+/** @typedef {import('zustand').State} State */
+/**
+ * @template {State} TState
+ * @typedef {import('zustand').StateCreator<TState>} StateCreator
+ */
+/**
+ * @template {State} TState
+ * @typedef {import('zustand').StoreApi<TState>} StoreApi
+ */
+
+/**
+ * bla
+ *
+ * @template {State} TState
+ * @param {StateCreator<TState> | StoreApi<TState>} state
+ *
+ */
+export function useSubState(state) {
+	return useRef(createStore(state)).current;
 }
