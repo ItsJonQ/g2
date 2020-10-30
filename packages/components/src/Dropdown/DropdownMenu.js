179fc7d6e328f92eb88cf8a15a4d9ee25d90e2f0
Jon Q <hello@jonquach.com>
2020-10-30 14:09:56 -0400

    Dropdown: Fix min/max width rendering

--- packages/components/src/Dropdown/DropdownMenu.js
+++ packages/components/src/Dropdown/DropdownMenu.js
@@ -21,7 +21,11 @@ function DropdownMenu(props, forwardedRef) {
 
 	const { label, menu } = useDropdownContext();
 
-	const classes = cx(styles.DropdownMenu, className);
+	const classes = cx(
+		styles.DropdownMenu,
+		css({ maxWidth, minWidth }),
+		className,
+	);
 
 	return (
 		<Menu
@@ -33,11 +37,7 @@ function DropdownMenu(props, forwardedRef) {
 			ref={forwardedRef}
 		>
 			{(menu?.visible || menu?.animating) && (
-				<DropdownMenuCard
-					elevation={elevation}
-					maxWidth={maxWidth}
-					minWidth={minWidth}
-				>
+				<DropdownMenuCard elevation={elevation}>
 					{children}
 				</DropdownMenuCard>
 			)}
@@ -45,18 +45,12 @@ function DropdownMenu(props, forwardedRef) {
 	);
 }
 
-const DropdownMenuCard = React.memo(
-	({ children, elevation, maxWidth, minWidth }) => {
-		const classes = cx(styles.Card, css({ maxWidth, minWidth }));
-
-		return (
-			<Card className={classes} elevation={elevation}>
-				<Scrollable className={styles.Scrollable}>
-					{children}
-				</Scrollable>
-			</Card>
-		);
-	},
-);
+const DropdownMenuCard = React.memo(({ children, elevation }) => {
+	return (
+		<Card className={styles.Card} elevation={elevation}>
+			<Scrollable className={styles.Scrollable}>{children}</Scrollable>
+		</Card>
+	);
+});
 
 export default contextConnect(DropdownMenu, 'DropdownMenu');
