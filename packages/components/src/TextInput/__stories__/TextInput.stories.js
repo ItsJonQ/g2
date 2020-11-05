4d75a4f99e4c38a4f9e3a80e77e1b6b49f441536
Sara Marcondes <saram@fastmail.com>
2020-11-05 11:28:22 -0800

    Add mad libs example (#87)

--- packages/components/src/TextInput/__stories__/TextInput.stories.js
+++ packages/components/src/TextInput/__stories__/TextInput.stories.js
@@ -74,3 +74,39 @@ export const inlineRendering = () => {
 		</VStack>
 	);
 };
+
+const MadLib = ({ type }) => <TextInput isInline placeholder={type} />;
+const Adjective = () => <MadLib type="Adjective" />;
+const Noun = () => <MadLib type="Noun" />;
+const PluralNoun = () => <MadLib isInline type="Plural noun" />;
+const Gerund = () => <MadLib isInline type="Gerund" />;
+
+/**
+ * Adapted from https://www.madlibs.com/printables/
+ */
+export const madLibs = () => (
+	<VStack
+		css={`
+			margin: auto;
+			text-align: justify;
+		`}
+	>
+		<Text adjustLineHeightForInnerControls>
+			A vacation is when you take a trip to some <Adjective /> place with
+			your <Adjective /> family. Usually you go to some place that is near
+			a/an <Noun /> or up on a/an <Noun />. A good vacation place is one
+			where you can ride <PluralNoun /> or play <MadLib type="Game" /> or
+			go birding for <MadLib type="Bird species" />. I like to spend my
+			time <Gerund /> or <Gerund />. When parents go on a vacation, they
+			spend their time eating three <PluralNoun /> a day. Last summer, my
+			little sibling fell in a/an <Noun /> and got poison{' '}
+			<MadLib type="Plant" /> all over their{' '}
+			<MadLib type="Part of the body" />. My family is going to go to
+			(the) <MadLib type="Place" />, and I will practice <Gerund />.
+			Parents need vacations more than kids because parents are always
+			very <Adjective /> and because they have to work{' '}
+			<MadLib type="Number" /> hours every day all year making enough{' '}
+			<PluralNoun /> to pay for the vacation.
+		</Text>
+	</VStack>
+);
