8b9b17565853cb23e0ca487469661ae8e9c008f1
Jon Q <hello@jonquach.com>
2020-11-08 10:36:23 -0500

    Fix RootStore resolver. Add JSDoc comments.

--- packages/create-styles/src/cssCustomProperties/createRootStore.js
+++ packages/create-styles/src/cssCustomProperties/createRootStore.js
@@ -1,6 +1,10 @@
 import { getPropValue } from './getPropValue';
 import { hasVariable } from './utils';
 
+/**
+ * Stores CSS config variables that are expected to be added to :root.
+ * This is used for CSS variable fallback handling for IE 11.
+ */
 class RootStore {
 	state = {};
 
@@ -8,34 +12,84 @@ class RootStore {
 		this.setState(initialState);
 	}
 
+	/**
+	 * Retrieve a value from the state.
+	 * @param {string} key The key to retrieve.
+	 * @returns {string} The value.
+	 */
 	get = (key) => this.state[key];
 
+	/**
+	 * Retrieves the current state.
+	 * @returns {object} The state.
+	 */
 	getState = () => ({ ...this.state });
 
+	/**
+	 * Sets the state.
+	 * @param {object} next The next state to merge into the current state.
+	 * @returns {object} The state.
+	 */
 	setState = (next = {}) => {
-		this.state = { ...this.state, ...next };
-		this.resolveVariablesInStateValue();
+		this._updateState(next);
+		this._resolveVariablesInStateValue();
+
+		return this.state;
 	};
 
-	resolveVariablesInStateValue = (pass = 0) => {
-		const next = {};
-		const values = Object.values(this.state).filter(hasVariable);
+	/**
+	 * Updates the state.
+	 * @param {object} next The next state to merge into the current state.
+	 */
+	_updateState = (next = {}) => {
+		this.state = { ...this.state, ...next };
+	};
 
-		for (const v of values) {
-			const [prop, value] = getPropValue(`resolve: ${v}`, this);
+	/**
+	 * Resolves potential CSS variables that may exist within the state's values.
+	 * This function cycles a maximum of 3 times to (hopefully) resolve chaining variable references.
+	 *
+	 * @param {number} pass The current pass (cycle) number.
+	 */
+	_resolveVariablesInStateValue = (pass = 0) => {
+		const next = {};
+
+		/**
+		 * Filter out entries so that we only target values with CSS variables.
+		 */
+		const entries = Object.entries(this.state).filter(([k, v]) =>
+			hasVariable(v),
+		);
+
+		for (const [k, v] of entries) {
+			const [, value] = getPropValue(`resolve: ${v}`, this.getState());
+			/**
+			 * Set the value for the next state, if available.
+			 */
 			if (value) {
-				next[prop] = value;
+				next[k] = value;
 			}
 		}
 
-		this.state = { ...this.state, ...next };
+		this._updateState(next);
 
-		if (pass === 2) {
-			this.resolveVariablesInStateValue(pass + 1);
+		/**
+		 * Run this function again, for a total of 3 passes.
+		 */
+		if (pass < 3) {
+			this._resolveVariablesInStateValue(pass + 1);
 		}
 	};
 }
 
+/**
+ * Creates a RootStore instance.
+ * This store contains a collection of CSS variables that is expected to
+ * be added to the :root {} node.
+ *
+ * @param {object} initialState The initial config.
+ * @returns {object} The RootStore instance.
+ */
 export function createRootStore(initialState = {}) {
 	const store = new RootStore(initialState);
 
