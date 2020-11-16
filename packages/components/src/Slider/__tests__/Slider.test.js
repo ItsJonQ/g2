3e380aac624bbe78439a46de5653aad7256cb2b8
Q <hello@jonquach.com>
2020-11-16 14:26:55 -0500

    Slider: Add support for unit handling (#122)
    * Slider: Allow for slider to understand units for incoming/outgoing values
    
    * Simplify typography panel example with new Slider update (which now understands units)

--- packages/components/src/Slider/__tests__/Slider.test.js
+++ packages/components/src/Slider/__tests__/Slider.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { Slider } from '../index';
@@ -25,7 +25,70 @@ describe('props', () => {
 	});
 
 	test('should render value', () => {
-		const { container } = render(<Slider id="slider" size="40" />);
+		const { container } = render(<Slider id="slider" value="40" />);
 		expect(container.firstChild).toMatchSnapshot();
+	});
+
+	test('should render unit value', () => {
+		const { container } = render(<Slider id="slider" value="40px" />);
+		const input = container.querySelector('input');
+
+		expect(container.firstChild).toMatchSnapshot();
+		expect(input.value).toEqual('40');
+	});
+
+	test('should include unit in onChange callback (if value contains unit)', () => {
+		let value = '40px';
+		const setValue = (next) => (value = next);
+
+		render(
+			<Slider
+				data-testid="slider"
+				id="slider"
+				onChange={setValue}
+				value={value}
+			/>,
+		);
+
+		const input = screen.getByTestId('slider');
+
+		fireEvent.change(input, { target: { value: 13 } });
+
+		// onChange callback value
+		expect(value).toBe('13px');
+	});
+
+	test('should change unit in onChange callback, if incoming value unit changes', () => {
+		let value = '40px';
+		const setValue = (next) => (value = next);
+
+		const { rerender } = render(
+			<Slider
+				data-testid="slider"
+				id="slider"
+				onChange={setValue}
+				value={value}
+			/>,
+		);
+
+		const input = screen.getByTestId('slider');
+
+		expect(input.value).toBe('40');
+
+		rerender(
+			<Slider
+				data-testid="slider"
+				id="slider"
+				onChange={setValue}
+				value="100%"
+			/>,
+		);
+
+		expect(input.value).toBe('100');
+
+		fireEvent.change(input, { target: { value: 13 } });
+
+		// onChange callback value
+		expect(value).toBe('13%');
 	});
 });
