82f4470ae0e710f0373d3336fb8b6d753610e1ce
Jon Q <hello@jonquach.com>
2020-11-17 20:30:30 -0500

    Rename files to kebabcase

--- packages/substate/src/index.js
+++ packages/substate/src/index.js
@@ -2,4 +2,4 @@ export { default as createStore } from 'zustand';
 export { default as shallowCompare } from 'zustand/shallow';
 export { default as createBaseStore } from 'zustand/vanilla';
 
-export * from './useSubState';
+export * from './use-sub-state';
