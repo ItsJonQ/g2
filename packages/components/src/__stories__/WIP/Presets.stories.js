cc4c4d26bafd9100c186a15eaa391e901a2b5b05
Jon Q <hello@jonquach.com>
2020-11-19 09:30:53 -0500

    Revert WIP/Preset stories

--- packages/components/src/__stories__/WIP/Presets.stories.js
+++ packages/components/src/__stories__/WIP/Presets.stories.js
@@ -1,31 +1,34 @@
 import { ui } from '@wp-g2/styles';
 import React from 'react';
 
-import { HStack, View } from '../../index';
+import { HStack, Spacer, View } from '../../index';
 
 export default {
 	title: 'Examples/WIP/Presets',
 };
 
+const Thing = ({ children = 'View', ...props }) => (
+	<View
+		{...props}
+		css={[
+			ui.font.default,
+			ui.background.green,
+			ui.padding(3),
+			ui.borderRadius.round,
+			ui.border.all,
+			ui.animation.ease,
+			ui.hover(ui.background.red, ui.scale(1.2)),
+			ui.mode.dark([`border: 3px solid white`]),
+			ui.$('NestedThing').css([`border: 2px solid black`]),
+		]}
+	>
+		{children}
+	</View>
+);
+
 const Tammie = () => {
 	return (
-		<View
-			css={[
-				ui.padding(5),
-				ui.background.blue,
-				ui.scale(1),
-				ui.hover(ui.scale(1.5), ui.background.orange),
-				ui.animation.default,
-				{
-					boxShadow: ui.flow(
-						ui.calc(ui.get('controlHeight'), '+', ui.value.px(40)),
-						ui.get('gridBase'),
-						ui.calc('calc(1px + 20px)', '+', 'calc(1px + 20px)'),
-						ui.get('colorAdmin'),
-					),
-				},
-			]}
-		>
+		<View css={[ui.scale(1), ui.hover(ui.scale(2)), ui.animation.default]}>
 			Tammie!
 			<span>TEA!</span>
 		</View>
