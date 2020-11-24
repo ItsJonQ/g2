8cfa5000867281228c5a6695386733d2d6e7afc6
Sara Marcondes <saram@fastmail.com>
2020-11-24 15:40:52 -0800

    Type Accordion

--- packages/components/src/Accordion/Accordion.js
+++ packages/components/src/Accordion/Accordion.js
@@ -5,6 +5,11 @@ import { AccordionContext } from './Accordion.Context';
 import { AccordionView } from './Accordion.styles';
 import { useAccordion } from './useAccordion';
 
+/**
+ *
+ * @param {import('@wp-g2/create-styles').ViewOwnProps<import('./useAccordion').Props, 'div'>} props
+ * @param {import('react').Ref<any>} forwardedRef
+ */
 function Accordion(props, forwardedRef) {
 	const { children, contextValue, ...otherProps } = useAccordion(props);
 
@@ -17,4 +22,7 @@ function Accordion(props, forwardedRef) {
 	);
 }
 
-export default contextConnect(Accordion, 'Accordion');
+/** @type {import('@wp-g2/create-styles').PolymorphicComponent<'div', import('./useAccordion').Props>} */
+const ConnectedAccordion = contextConnect(Accordion, 'Accordion');
+
+export default ConnectedAccordion;
--- packages/components/src/Accordion/useAccordion.js
+++ packages/components/src/Accordion/useAccordion.js
@@ -3,10 +3,21 @@ import { shallowCompare, useSubState } from '@wp-g2/substate';
 import { is, noop, simpleEqual, uniq, useUpdateEffect } from '@wp-g2/utils';
 import { useCallback, useEffect, useMemo } from 'react';
 
+/**
+ * @param {any} current
+ * @returns {curent is any[] | string}
+ */
 const isCurrentValid = (current) => is.array(current) || is.string(current);
 
+/**
+ * @param {string[]} next
+ */
 const sanitizeState = (next) => uniq(next.filter(Boolean));
 
+/**
+ * @param {string[]} prev
+ * @param {string | string[]} next
+ */
 const setCurrentState = (prev = [], next) => {
 	if (!isCurrentValid(next)) return prev;
 	const nextState = is.array(next) ? [...prev, ...next] : [...prev, next];
@@ -14,6 +25,38 @@ const setCurrentState = (prev = [], next) => {
 	return sanitizeState(nextState);
 };
 
+/**
+ * @typedef AccordionStore
+ * @property {string[]} current
+ * @property {(next: string[] | string) => void} add
+ * @property {(next: string) => void} remove
+ * @property {(next: string[] | string) => void} set
+ * @property {() => void} clear
+ * @property {(id: string | undefined) => boolean} has
+ * @property {(next: string[] | string) => boolean} isEqual
+ * @property {(id: string | undefined) => boolean} isVisible
+ */
+
+/**
+ * @typedef {[boolean, (next: boolean) => void]} AccordionState
+ */
+
+/**
+ * @typedef Store
+ * @property {(options: { id?: string, visible?: boolean }) => void} update
+ * @property {(options: { id?: string, visible?: boolean }) => AccordionState} useAccordionState
+ */
+
+/**
+ * @typedef Props
+ * @property {boolean} [allowMultiple=false]
+ * @property {(current: string[] | null) => void} [onChange]
+ * @property {string[]} current
+ */
+
+/**
+ * @param {Props} props
+ */
 export function useAccordion(props) {
 	const {
 		allowMultiple = false,
@@ -22,12 +65,15 @@ export function useAccordion(props) {
 		...otherProps
 	} = useContextSystem(props, 'Accordion');
 
-	const accordionStore = useSubState((set) => ({
+	/** @type {import('zustand').UseStore<AccordionStore>} */
+	const accordionStore = useSubState((
+		/** @type {import('zustand').SetState<AccordionStore>} */ set,
+	) => ({
 		// State
 		current: setCurrentState([], current),
 
 		// Actions
-		add: (next) => {
+		add: (/** @type {string[] | string} */ next) => {
 			if (!isCurrentValid(next)) return;
 
 			set((prev) => {
@@ -35,7 +81,7 @@ export function useAccordion(props) {
 			});
 		},
 
-		remove: (next) => {
+		remove: (/** @type {string} */ next) => {
 			if (!isCurrentValid(next)) return;
 
 			set((prev) => {
@@ -47,7 +93,7 @@ export function useAccordion(props) {
 			});
 		},
 
-		set: (next) => {
+		set: (/** @type {string[] | string} */ next) => {
 			if (!isCurrentValid(next)) return;
 			if (accordionStore.getState().isEqual(next)) return;
 
@@ -65,51 +111,54 @@ export function useAccordion(props) {
 		},
 
 		// Selectors
-		has: (id) => accordionStore.getState().current.includes(id),
-		isEqual: (next) => simpleEqual(accordionStore.getState().current, next),
-		isVisible: (id) => !!accordionStore.getState().has(id),
+		has: (/** @type {string | undefined} */ id) =>
+			!!id && accordionStore.getState().current.includes(id),
+		isEqual: (/** @type {string[] | string} */ next) =>
+			simpleEqual(accordionStore.getState().current, next),
+		isVisible: (/** @type {string | undefined} */ id) =>
+			!!accordionStore.getState().has(id),
 	}));
 
-	const store = useSubState(
-		() => ({
-			// Actions
-			update: ({ id, visible = false }) => {
-				if (visible) {
-					if (allowMultiple) {
-						accordionStore.getState().add(id);
-					} else {
-						accordionStore.getState().set(id);
-					}
+	/** @type {import('zustand').UseStore<Store>} */
+	const store = useSubState(() => ({
+		// Actions
+		update: ({ id, visible = false }) => {
+			if (!id) return;
+
+			if (visible) {
+				if (allowMultiple) {
+					accordionStore.getState().add(id);
 				} else {
-					if (allowMultiple) {
-						accordionStore.getState().remove(id);
-					} else {
-						// accordionStore.getState().clear();
-					}
+					accordionStore.getState().set(id);
 				}
-			},
-			useAccordionState: ({ id, visible }) => {
-				const state = accordionStore(
-					(prev) => prev.isVisible(id),
-					shallowCompare,
-				);
-				const setState = useCallback(
-					(next) => {
-						store.getState().update({ id, visible: next });
-					},
-					[id],
-				);
+			} else {
+				if (allowMultiple) {
+					accordionStore.getState().remove(id);
+				} else {
+					// accordionStore.getState().clear();
+				}
+			}
+		},
+		useAccordionState: ({ id, visible }) => {
+			const state = accordionStore(
+				(prev) => prev.isVisible(id),
+				shallowCompare,
+			);
+			const setState = useCallback(
+				(/** @type {boolean} */ next) => {
+					store.getState().update({ id, visible: next });
+				},
+				[id],
+			);
 
-				// Sync controlled state
-				useUpdateEffect(() => {
-					store.getState().update({ id, visible });
-				}, [setState, visible]);
+			// Sync controlled state
+			useUpdateEffect(() => {
+				store.getState().update({ id, visible });
+			}, [setState, visible]);
 
-				return [state, setState];
-			},
-		}),
-		[],
-	);
+			return [state, setState];
+		},
+	}));
 
 	// Sync incoming current prop
 	useUpdateEffect(() => {
