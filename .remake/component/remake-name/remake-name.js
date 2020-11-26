bde4cf1c04514c47f3f09f67eacac8e243be2ef6
Sara Marcondes <saram@fastmail.com>
2020-11-26 15:03:05 -0800

    Add typings to remake

--- .remake/component/remake-name/remake-name.js
+++ .remake/component/remake-name/remake-name.js
@@ -4,10 +4,21 @@ import React from 'react';
 import { <%= name %>View } from './<%= name %>.styles';
 import * as styles from './<%= name %>.styles';
 
+/**
+ * @param {import('@wp-g2/create-styles').ViewOwnProps<import('./types').Props, 'div'>}
+ * @param {import('react').Ref<any>}
+ */
 function <%= name %> (props, forwardedRef) {
 	const {...otherProps} = useContextSystem(props, '<%= name %>')
 	
 	return <<%= name %>View {...otherProps } />;
 }
 
-export default contextConnect(<%= name %>, '<%= name %>');
+/**
+ * `<%= name %>`
+ * 
+ * @type {import('@wp-g2/create-styles').PolymorphicComponent<'div', import('./types').Props>}
+ */
+const Connected<%= name %> = contextConnect(<%= name %>, '<%= name %>');
+
+export default Connected<%= name %>;
