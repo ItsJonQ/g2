edf6081d296abf0a4ab2aeb54f016d774fa4b813
Sara Marcondes <saram@fastmail.com>
2020-11-27 08:28:08 -0800

    Add typings to remake (#174)
    * Add typings to remake
    
    * Add missing parameter names
    
    * Add example to docs

--- .remake/component/remake-name/remake-name.js
+++ .remake/component/remake-name/remake-name.js
@@ -4,10 +4,32 @@ import React from 'react';
 import { <%= name %>View } from './<%= name %>.styles';
 import * as styles from './<%= name %>.styles';
 
+/**
+ * @param {import('@wp-g2/create-styles').ViewOwnProps<import('./types').Props, 'div'>} props
+ * @param {import('react').Ref<any>} forwardedRef
+ */
 function <%= name %> (props, forwardedRef) {
 	const {...otherProps} = useContextSystem(props, '<%= name %>')
 	
 	return <<%= name %>View {...otherProps } />;
 }
 
-export default contextConnect(<%= name %>, '<%= name %>');
+/**
+ * `<%= name %>`
+ *
+ * @example
+ * ```jsx
+ * import { <%= name %> } from '@wp-g2/components'
+ *
+ * function Example() {
+ *   return (
+ *     <<%= name %> />
+ *   );
+ * }
+ * ```
+ *
+ * @type {import('@wp-g2/create-styles').PolymorphicComponent<'div', import('./types').Props>}
+ */
+const Connected<%= name %> = contextConnect(<%= name %>, '<%= name %>');
+
+export default Connected<%= name %>;
