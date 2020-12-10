7ad6bc2e3dd45bf20d74e3dacf26ef824c0781f8
Jon Q <hello@jonquach.com>
2020-12-09 19:10:59 -0500

    Update snapshots

--- packages/styles/src/system.js
+++ packages/styles/src/system.js
@@ -155,6 +155,7 @@ const systemConfig = {
 		WebkitFontSmoothing: 'antialiased',
 		fontFamily: getConfig('fontFamily'),
 		fontSize: getConfig('fontSize'),
+		// @ts-ignore
 		fontWeight: getConfig('fontWeight'),
 		margin: 0,
 	},
