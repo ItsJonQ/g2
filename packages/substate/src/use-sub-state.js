b5e2ac5ebdead859a75033754d96aa2e3d5f34e9
Jon Q <hello@jonquach.com>
2021-01-11 15:20:03 -0500

    Fork Zustand (3.3.1) into @wp-g2/substate

--- packages/substate/src/use-sub-state.js
+++ packages/substate/src/use-sub-state.js
@@ -1,9 +1,10 @@
 import { useRef } from 'react';
-import createStore from 'zustand';
+
+import createStore from './zustand';
 
 /**
  * @template {Record<string | number | symbol, unknown>} TState
- * @param {import('zustand').StateCreator<TState> | import('zustand').StoreApi<TState>} createState
+ * @param {import('./zustand/types').StateCreator<TState> | import('./zustand/types').StoreApi<TState>} createState
  */
 export function useSubState(createState) {
 	return useRef(createStore(createState)).current;
