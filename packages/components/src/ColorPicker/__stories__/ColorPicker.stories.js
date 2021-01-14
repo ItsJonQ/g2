d5972726afedc1f2e03b27d6962fc8ff0d23f1cd
Jon Q <hello@jonquach.com>
2021-01-14 09:07:40 -0500

    Adjust ColorPicker HTML story

--- packages/components/src/ColorPicker/__stories__/ColorPicker.stories.js
+++ packages/components/src/ColorPicker/__stories__/ColorPicker.stories.js
@@ -1,7 +1,7 @@
 import { ui } from '@wp-g2/styles';
 import React from 'react';
 
-import { Container, Grid, VStack } from '../../index';
+import { Container, Grid, Heading, VStack } from '../../index';
 import { ColorPicker } from '../index';
 
 export default {
@@ -61,32 +61,28 @@ export const _default = () => {
 };
 
 export const HTMLPickerTest = () => {
-	const [color, setColor] = React.useState('red');
-
-	const handleOnChange = (next, data) => {
-		// console.log(data);
-		setColor(next);
-	};
+	const [color, setColor] = React.useState('#ff0000');
 
 	return (
-		<Container>
-			<p>
-				<strong>{color}</strong>
-			</p>
-			<VStack spacing={10}>
-				<Grid gap={8}>
-					<ColorPicker
-						color={color}
-						onChange={handleOnChange}
-						width={300}
-					/>
-					<input
-						onChange={(e) => setColor(e.target.value)}
-						type="color"
-						value={ui.color(color).toHexString()}
-					/>
-				</Grid>
+		<VStack>
+			<VStack>
+				<VStack>
+					<VStack>
+						<VStack>
+							<Heading size={1}>
+								<span aria-label="wave" role="img">
+									👋
+								</span>{' '}
+								{color}
+							</Heading>
+							<input
+								onChange={(e) => setColor(e.target.value)}
+								type="color"
+							/>
+						</VStack>
+					</VStack>
+				</VStack>
 			</VStack>
-		</Container>
+		</VStack>
 	);
 };
