da970603167077623134ff1a7c6ffb6383dd1bfa
Jon Q <hello@jonquach.com>
2021-01-17 11:20:39 -0500

    Improve keyboard update support

--- packages/components/src/ColorPicker/ColorPickerElement.js
+++ packages/components/src/ColorPicker/ColorPickerElement.js
@@ -6,7 +6,19 @@ import { useColorPickerContext } from './ColorPicker.Context';
 export const ColorPickerElement = React.memo(({ width }) => {
 	const { colorRgb, onChange } = useColorPickerContext();
 
+	/**
+	 * Focuses the interactive control from the ColorPicker library.
+	 * This allows for a smoother mouse -> keyboard update flow.
+	 */
+	const handleOnFocusInteractiveControl = (event) => event.target.focus();
+
 	return (
-		<RgbaColorPicker color={colorRgb} onChange={onChange} width={width} />
+		<div onMouseDown={handleOnFocusInteractiveControl}>
+			<RgbaColorPicker
+				color={colorRgb}
+				onChange={onChange}
+				width={width}
+			/>
+		</div>
 	);
 });
