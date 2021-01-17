e2f03b9b4d225af6f6bd47df5ee2424008abcca5
Jon Q <hello@jonquach.com>
2021-01-17 11:02:58 -0500

    Update to the latest react-colorful (v4.4.4)

--- packages/components/src/ColorPicker/ColorPicker.styles.js
+++ packages/components/src/ColorPicker/ColorPicker.styles.js
@@ -1,6 +1,5 @@
 import { css, styled, ui } from '@wp-g2/styles';
 
-const colorControlHeight = 16;
 const alphaPatternSvg =
 	'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill-opacity=".05"><rect x="8" width="8" height="8"/><rect y="8" width="8" height="8"/></svg>';
 
@@ -14,45 +13,23 @@ export const ColorPickerView = styled.div`
 		display: flex;
 		flex-direction: column;
 		height: 100%;
+		position: relative;
 		min-height: 128px;
 		user-select: none;
 		width: 100%;
 	}
 
-	.react-colorful,
 	.react-colorful__saturation {
 		position: relative;
-	}
-
-	.react-colorful__hue {
-		border-top-left-radius: ${ui.get('controlBorderRadius')};
-		border-top-right-radius: ${ui.get('controlBorderRadius')};
-		order: 1;
-	}
-
-	.react-colorful__saturation {
-		border-radius: 0;
-		order: 2;
-	}
-
-	.react-colorful__alpha {
-		border-bottom-left-radius: ${ui.get('controlBorderRadius')};
-		border-bottom-right-radius: ${ui.get('controlBorderRadius')};
-		order: 3;
-	}
-
-	.react-colorful__saturation {
 		flex-grow: 1;
-		/* border-bottom: ${ui.value.px(colorControlHeight / 2)} solid #000; */
-		background-image: linear-gradient(0deg, #000, transparent),
-			linear-gradient(90deg, #fff, hsla(0, 0%, 100%, 0));
+		border-bottom: 8px solid #000;
+		border-radius: 0;
+		background-image: linear-gradient(to top, #000, rgba(0, 0, 0, 0)),
+			linear-gradient(to right, #fff, rgba(255, 255, 255, 0));
 	}
 
-	._1pdeC,
-	.react-colorful__alpha-pointer:after,
-	.react-colorful__hue-pointer:after,
-	.react-colorful__saturation-pointer:after,
-	.react-colorful__alpha:after {
+	.react-colorful__pointer-fill,
+	.react-colorful__alpha-gradient {
 		content: '';
 		position: absolute;
 		left: 0;
@@ -63,94 +40,88 @@ export const ColorPickerView = styled.div`
 		border-radius: inherit;
 	}
 
-	.react-colorful__saturation,
-	.react-colorful__alpha:after {
+	/* Improve elements rendering on light backgrounds */
+	.react-colorful__alpha-gradient,
+	.react-colorful__saturation {
 		box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.05);
 	}
 
 	.react-colorful__hue,
 	.react-colorful__alpha {
 		position: relative;
-		height: ${ui.value.px(colorControlHeight)};
+		height: 16px;
 	}
 
 	.react-colorful__hue {
+		border-radius: 3px 3px 0 0;
 		background: linear-gradient(
-			90deg,
-			red,
+			to right,
+			#f00 0%,
 			#ff0 17%,
 			#0f0 33%,
 			#0ff 50%,
 			#00f 67%,
 			#f0f 83%,
-			red
+			#f00 100%
 		);
 	}
 
-	._3nKPH {
+	.react-colorful__last-control {
+		border-radius: 0 0 8px 8px;
+	}
+
+	.react-colorful__interactive {
 		position: absolute;
 		left: 0;
 		top: 0;
 		right: 0;
 		bottom: 0;
 		border-radius: inherit;
-		touch-action: none;
 		outline: none;
+		/* Don't trigger the default scrolling behavior when the event is originating from this element */
+		touch-action: none;
 	}
 
-	.react-colorful__saturation {
-		._3nKPH {
-			top: 0;
-			bottom: 0;
-		}
-	}
-
-	._3nKPH:focus {
-		.react-colorful__alpha-pointer,
-		.react-colorful__hue-pointer,
-		.react-colorful__saturation-pointer {
-			transform: translate(-50%, -50%) scale(1.1);
-		}
-	}
-
-	.react-colorful__alpha-pointer,
-	.react-colorful__hue-pointer,
-	.react-colorful__saturation-pointer {
+	.react-colorful__pointer {
 		position: absolute;
 		z-index: 1;
 		box-sizing: border-box;
-		height: ${ui.value.px(colorControlHeight + 4)};
-		width: ${ui.value.px(colorControlHeight + 4)};
+		width: 20px;
+		height: 20px;
 		transform: translate(-50%, -50%);
 		background-color: #fff;
 		border: 2px solid #fff;
 		border-radius: 50%;
 		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-
-		&:after {
-			background-color: currentColor;
-		}
 	}
 
-	.react-colorful__alpha {
+	.react-colorful__interactive:focus .react-colorful__pointer {
+		transform: translate(-50%, -50%) scale(1.1);
+	}
+
+	/* Chessboard-like pattern for alpha related elements */
+	.react-colorful__alpha,
+	.react-colorful__alpha-pointer {
 		background-color: #fff;
-		background-image: url('${alphaPatternSvg}');
+		background-image: url(${alphaPatternSvg});
+	}
+
+	/* Display the saturation pointer over the hue one */
+	.react-colorful__saturation-pointer {
+		z-index: 3;
+	}
+
+	/* Display the hue pointer over the alpha one */
+	.react-colorful__hue-pointer {
+		z-index: 2;
+	}
+
+	.react-colorful__hue {
+		order: 0;
 	}
 
 	.react-colorful__saturation {
-		.react-colorful__alpha-pointer,
-		.react-colorful__hue-pointer,
-		.react-colorful__saturation-pointer {
-			z-index: 3;
-		}
-	}
-
-	.react-colorful__hue ._3vRUF {
-		.react-colorful__alpha-pointer,
-		.react-colorful__hue-pointer,
-		.react-colorful__saturation-pointer {
-			z-index: 2;
-		}
+		order: 1;
 	}
 `;
 
