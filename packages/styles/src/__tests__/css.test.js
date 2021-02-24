6b1d6ea477cb95cddefc64d6fc414a727fd83415
sarayourfriend <saram@fastmail.com>
2021-02-24 11:11:01 -0800

    Add automagical interpolation of StyleQueries (#271)

--- packages/styles/src/__tests__/css.test.js
+++ packages/styles/src/__tests__/css.test.js
@@ -1,5 +1,46 @@
-import { getScaleStyles } from '../css';
+import { css, getScaleStyles } from '../css';
 import { space } from '../mixins/space';
+import { $ as getStyleQuery } from '../presets/style-query';
+
+describe('css', () => {
+	const getLastAppliedCssRule = () => {
+		const styles = document.getElementsByTagName('style');
+		const lastSheet = Array.from(styles).slice(-1)[0];
+		const rules = Array.from(lastSheet.sheet.cssRules);
+		return rules.slice(-1)[0];
+	};
+
+	beforeEach(() => {
+		// clean up generated styles and elements
+		document.head.innerHTML = '';
+		document.body.innerHTML = '';
+	});
+
+	it('should interpolate style queries', () => {
+		const classes = css`
+			color: red;
+			${getStyleQuery('TestComponent')} {
+				color: black;
+			}
+		`;
+
+		const rule = getLastAppliedCssRule();
+
+		/**
+		 * Generate the following:
+		 * <View className={classes}>
+		 * 	<TestComponent />
+		 * </View>
+		 */
+		const container = document.createElement('div');
+		const testElement = document.createElement('div');
+		container.className = classes;
+		testElement.dataset.g2Component = 'TestComponent';
+		container.appendChild(testElement);
+
+		expect(testElement.matches(rule.selectorText)).toBe(true);
+	});
+});
 
 describe('scales', () => {
 	test('should transform space values', () => {
