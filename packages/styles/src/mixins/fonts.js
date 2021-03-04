4f5d1c143833c4c96ef5b60b7780a13d5c007061
sarayourfriend <saram@fastmail.com>
2021-03-04 11:52:25 -0800

    Fix `getHeadingFontSize` types (#279)
    * Fix `getHeadingFontSize` types
    
    * Ensure non-numbers are handled correctly in getFontSize
    
    * Add tests
    
    * Update snapshot tests

--- packages/styles/src/mixins/fonts.js
+++ packages/styles/src/mixins/fonts.js
@@ -2,7 +2,7 @@ import { get } from '../core';
 
 export const BASE_FONT_SIZE = 13;
 
-const PRESET_FONT_SIZES = {
+export const PRESET_FONT_SIZES = {
 	body: 13,
 	caption: 10,
 	footnote: 11,
@@ -11,7 +11,10 @@ const PRESET_FONT_SIZES = {
 	title: 20,
 };
 
-const HEADING_FONT_SIZES = [1, 2, 3, 4, 5, 6];
+export const HEADING_FONT_SIZES = [1, 2, 3, 4, 5, 6].flatMap((n) => [
+	n,
+	n.toString(),
+]);
 
 /**
  *
@@ -23,12 +26,20 @@ export function getFontSize(size = BASE_FONT_SIZE) {
 		return getFontSize(PRESET_FONT_SIZES[size]);
 	}
 
-	if (typeof size !== 'number') return size;
+	if (typeof size !== 'number') {
+		const parsed = parseFloat(size);
+		if (Number.isNaN(parsed)) return size;
+		size = parsed;
+	}
 
-	const ratio = /** @type {number} */ (size) / BASE_FONT_SIZE;
+	const ratio = `(${size} / ${BASE_FONT_SIZE})`;
 	return `calc(${ratio} * ${get('fontSize')})`;
 }
 
+/**
+ * @param {import('react').ReactText} size
+ * @return {string}
+ */
 export function getHeadingFontSize(size = 3) {
 	if (!HEADING_FONT_SIZES.includes(size)) {
 		return getFontSize(size);
