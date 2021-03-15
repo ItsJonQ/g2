cb47463937c25d3acf3d0bc6a25e1f648eda44e5
Kai Hao <kevin830726@gmail.com>
2021-03-15 21:44:01 +0800

    Add type=button by default to Button component (#288)

--- packages/components/src/Button/__tests__/Button.test.js
+++ packages/components/src/Button/__tests__/Button.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { Button } from '../index';
@@ -97,5 +97,17 @@ describe('props', () => {
 			<Button variant="primary">Let It Go</Button>,
 		);
 		expect(container.firstChild).toMatchSnapshot();
+	});
+
+	test('should render type', () => {
+		render(
+			<>
+				<Button type="submit">Submit</Button>
+				<Button href="#">Link</Button>
+			</>,
+		);
+
+		expect(screen.getByRole('button')).toHaveAttribute('type', 'submit');
+		expect(screen.getByRole('link')).not.toHaveAttribute('type');
 	});
 });
